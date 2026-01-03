fec120ec5f2d542a49ecd10ee14dc1fcd3ae5ba1
panditbimlendra <panditbimlendra10@gmail.com>
2026-01-04 01:47:32 +0545

    added files

---
+++ push_large_files.py
@@ -0,0 +1,11 @@
+# step1
+# git lfs migrate import --include="Mid term/backend/**/*.ckpt,Mid term/backend/**/*.pt" --include-ref=refs/heads/main
+
+
+
+# step2
+# git push origin main --force
+
+
+
+
